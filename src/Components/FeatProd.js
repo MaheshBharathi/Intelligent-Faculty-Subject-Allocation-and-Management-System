f3d5b649cd89409d532bb034537e38da8df84e00
MaheshBharathi <maheshbharathi2005@gmail.com>
2025-03-05 11:40:57 +0530

    Update FeatProd.js

--- src/Components/FeatProd.js
+++ src/Components/FeatProd.js
@@ -1,9 +1,9 @@
-import React from 'react'
-function FeatProd() {
-  return (
-    <div>
-        Featured Products
-    </div>
-  )
-}
-export default FeatProd
+//import React from 'react'
+//function FeatProd() {
+  //return (
+    //<div>
+       // Featured Products
+    //</div>
+  //)
+//}
+//export default FeatProd
