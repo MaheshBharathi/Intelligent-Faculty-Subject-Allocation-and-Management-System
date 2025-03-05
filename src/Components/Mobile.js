dbdad22a9baa57b8f14f1fd3959edc190f874456
MaheshBharathi <maheshbharathi2005@gmail.com>
2025-03-05 11:44:42 +0530

    Update Mobile.js

--- src/Components/Mobile.js
+++ src/Components/Mobile.js
@@ -1,10 +1,10 @@
-import React from 'react'
+//import React from 'react'
  
-function Mobile() {
-  return (
-    <div>
+//function Mobile() {
+  //return (
+    //<div>
 
-    </div>
-  )
-}
-export default Mobile
+    /</div>
+  //)
+//}
+//export default Mobile
